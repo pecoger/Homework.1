eaf76708a71c577e7be89cbc3219838159c700f6
Petko Gerdemski <peco20@abv.bg>
2022-01-28 16:14:43 +0200

    code format for easy reading

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+V
+&src/main/java/homeWork1/homeWork1.java,3\5\355d669a34bd861b57820108670510ef3ceb95e1
--- src/main/java/homeWork1/homeWork1.java
+++ src/main/java/homeWork1/homeWork1.java
@@ -7,26 +7,20 @@ public class homeWork1 {
     public static void main(String[] args) {
 
         System.out.println("Please enter your name");
-
         Scanner scanner = new Scanner(System.in);
-
         String firstName = scanner.next();
 
         System.out.println("Please enter last name");
-
         String lastName = scanner.next();
 
         System.out.println("Please enter your age");
-
         int userAge = scanner.nextInt();
 
         System.out.println("Please tell us about your hobbies");
-
         String hobbies = scanner.next();
 
         System.out.println("You are " + firstName + " " + lastName + " and your age is " + userAge +
                 "\nYour hobbies are: " + hobbies);
-
 
 
     }
